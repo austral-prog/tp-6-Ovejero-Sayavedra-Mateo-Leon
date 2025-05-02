1a1c31419cd9854e3426f8efef59cfd764d9bb83
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-05-02 18:23:54 -0300

    lists.py

--- lists.py
+++ lists.py
@@ -1,5 +1,3 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
     if len(list_to_remove_elements) >= 6:
         del list_to_remove_elements[5]
@@ -19,18 +17,18 @@ def is_empty (list_to_check):
         return True
     else:
         return False
-
-def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
-        if list_to_compare1[2] == list_to_compare2[2]:
-            return True
-        else:
-            return False
+        
+def check_list(list_to_compare1, list_to_compare2):
+    element3_list1 = list_to_compare1[2]
+    element3_list2 = list_to_compare2[2]
+    if element3_list1 == element3_list2:
+        return True
     else:
         return False
 
-def list_of_lists(list_of_lists_to_modify):
-    list_of_lists_to_modify[0]= list_of_lists_to_modify[0][:2]
-    list_of_lists_to_modify[1]= list_of_lists_to_modify[1][1:4]
-    list_of_lists_to_modify[2]= list_of_lists_to_modify[2][len(list_of_lists_to_modify[2])-2:len(list_of_lists_to_modify[2])]
+def list_to_list(list_of_lists_to_modify):
+    del list_of_lists_to_modify[0][2]    # Elimina 3 de [1, 2, 3]
+    del list_of_lists_to_modify[1][0]    # Elimina 4 de [4, 5, 6, 7, 8]
+    del list_of_lists_to_modify[1][-1]   # Elimina el último (8) de [5, 6, 7, 8]
+    list_of_lists_to_modify[2][0:2] = [] # Elimina 9 de [9, 10, 11, 12]
     return list_of_lists_to_modify
