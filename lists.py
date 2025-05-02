fc3e962b6d3f7d8ee33f85116ccc97fb397dd48e
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-05-02 17:20:00 -0300

    lists.py

--- lists.py
+++ lists.py
@@ -1,26 +1,21 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    lista = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-    lista[4:6] = []
-    del lista [0]
-    return lista
-
+    list_to_remove_elements[4:6] = []
+    del list_to_remove_elements [0]
+    return list_to_remove_elements
 
 def add_elements(list_to_add_elements):
-    lista = ['Red', 'Green', 'White', 'Black']
-    lista.insert(0, "Pink")
-    lista.append('Yellow')
-    return lista
-
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.append('Yellow')
+    return list_to_add_elements
 
 def is_empty(list_to_check):
     if (len(list_to_check) == 0):
         return True
     else:
         return False
-
-
+        
 def check_lists(list_to_compare1, list_to_compare2):
     element3_list1 = list_to_compare1[2]
     element3_list2 = list_to_compare2[2]
@@ -28,7 +23,6 @@ def check_lists(list_to_compare1, list_to_compare2):
         return True
     else:
         return False
-
 
 def list_of_lists(list_of_lists_to_modify):
     del list_of_lists_to_modify[0][2]
