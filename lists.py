958956f98a1e5e6621a952edc984e7df0681d59e
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-05-02 17:34:34 -0300

    lists.py

--- lists.py
+++ lists.py
@@ -1,32 +1,40 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    list_to_remove_elements[4:6] = []
-    del list_to_remove_elements [0]
+    if len(list_to_remove_elements) >= 6:
+        del list_to_remove_elements[5]
+    if len(list_to_remove_elements) >= 5:
+        del list_to_remove_elements[4]
+    if len(list_to_remove_elements) >= 1:
+        del list_to_remove_elements[0]
     return list_to_remove_elements
+
 
 def add_elements(list_to_add_elements):
     list_to_add_elements.insert(0, "Pink")
     list_to_add_elements.append('Yellow')
     return list_to_add_elements
 
+
 def is_empty(list_to_check):
-    if (len(list_to_check) == 0):
-        return True
-    else:
-        return False
-        
-def check_lists(list_to_compare1, list_to_compare2):
-    element3_list1 = list_to_compare1[2]
-    element3_list2 = list_to_compare2[2]
-    if element3_list1 == element3_list2:
+    if len(list_to_check) == 0:
         return True
     else:
         return False
 
+
+def check_lists(list_to_compare1, list_to_compare2):
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 def list_of_lists(list_of_lists_to_modify):
-    del list_of_lists_to_modify[0][2]
-    del list_of_lists_to_modify[1][0]
-    del list_of_lists_to_modify[1][-1]
-    list_of_lists_to_modify[2][0:2] = []
+    list_of_lists_to_modify[0]= list_of_lists_to_modify[0][:2]
+    list_of_lists_to_modify[1]= list_of_lists_to_modify[1][1:4]
+    list_of_lists_to_modify[2]= list_of_lists_to_modify[2][len(list_of_lists_to_modify[2])-2:len(list_of_lists_to_modify[2])]
     return list_of_lists_to_modify
